ae94b4ca793d51b8ba8143c4b6c669c1286b23bc
Sandeep Kumar Gangarapu <sandeepgangarapu@users.noreply.github.com>
2020-09-09 14:12:31 -0700

    small changes

--- run_sim.py
+++ run_sim.py
@@ -7,11 +7,11 @@ if __name__ == '__main__':
     true_means = [0.25, 1.82, 1.48, 2.25, 2]
     true_vars = [2.84,  1.97, 2.62, 1, 2.06]
 
-    alg_list=['thomp']
+    alg_list=['ab', 'thomp_inf', 'ucb', 'thomp']
     #
     # estimator_list=['aipw', 'eval_aipw', 'ipw'],
 
-    sim = BanditSimulation(num_ite=5, arm_means=true_means,
+    sim = BanditSimulation(num_ite=20, arm_means=true_means,
                            arm_vars=true_vars,
                            eps_inf=0.2,
                            horizon=2000,
