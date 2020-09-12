65425c9426c27bcec515841f79fe8305fd4904c9
Sandeep Kumar Gangarapu <sandeepgangarapu@users.noreply.github.com>
2020-09-11 18:09:53 -0700

    small changes

--- run_sim.py
+++ run_sim.py
@@ -7,13 +7,11 @@ def run_sim(file_path):
     start_time = time.time()
     true_means = np.random.uniform(0,3,5)
     true_vars = np.random.uniform(0,3,5)
-    print(true_means)
-    print(true_vars)
     alg_list=['thomp', 'thomp_inf']
 
     estimator_list=['aipw', 'eval_aipw', 'ipw']
 
-    sim = BanditSimulation(num_ite=2, arm_means=true_means,
+    sim = BanditSimulation(num_ite=1000, arm_means=true_means,
                            arm_vars=true_vars,
                            eps_inf=0.2,
                            horizon=500,
@@ -29,7 +27,7 @@ def run_sim(file_path):
     return true_means, true_vars
 
 if __name__ == '__main__':
-    num_meta_ite = 2
+    num_meta_ite = 100
     ref_lis = []
     for i in range(num_meta_ite):
         file_path = 'analysis/output/sim_weighed_ite_1000_t_500_metaite_' + str(i) + '.csv'
