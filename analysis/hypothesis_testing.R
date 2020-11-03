0ba9f35e6c2f684f767a2248b71d8e5133780d1a
Sandeep Kumar Gangarapu <sandeepgangarapu@users.noreply.github.com>
2020-11-03 18:20:31 +0530

    changes and UMAA

--- analysis/hypothesis_testing.R
+++ analysis/hypothesis_testing.R
@@ -6,10 +6,10 @@ true_means = c(0, 0.2)
 
 #data <- read.csv("hyp_ite_310_t_1052.csv") %>% drop_na() 
 #data <- read.csv("hyp_ite_310_t_170.csv") %>% drop_na() 
-data <- read.csv("hyp_ite_310_t_66.csv") %>% drop_na() 
+data <- read.csv("hyp_ite_1000_t_2000.csv") %>% drop_na() 
 
-data_0 <- data %>% filter(group==0) 
-data_1 <- data %>% filter(group==1)
+data_0 <- data %>% filter(group==3) 
+data_1 <- data %>% filter(group==4)
 
 a <- data_0[data_0$alg=='thomp_eval_aipw_var', 'var_est']
 data_0[data_0$alg=='thomp_eval_aipw', 'var_est'] = a
@@ -31,4 +31,6 @@ hyp_data <- merged_data %>%
          df = arm_pull_tracker.x+arm_pull_tracker.y-2,
          pval = dt(tval, df), test_pass = ifelse(pval<0.05, TRUE, FALSE))
 
-final_results <- hyp_data %>% filter(!grepl("var", alg) & test_pass !=TRUE) %>% group_by(alg, test_pass) %>% summarise(cnt = n()/310)
+final_results <- hyp_data %>% filter(!grepl("var", alg) & test_pass !=TRUE) %>%
+  group_by(alg, test_pass) %>% summarise(cnt = n(), perc = n()/1000)
+
--- analysis/wise.R
+++ analysis/wise.R
@@ -4,12 +4,13 @@ suppressPackageStartupMessages(
     library(dplyr)
     library(tidyr)
   })
+require(gridExtra)
 
 
 setwd("G:\\My Drive\\Research\\Bandits\\code\\bandits\\analysis\\output")
 
 
-data <- read.csv("wise_mse_graph.csv")
+data <- read.csv("wise_mse_graph.csv") %>% mutate(alg = ifelse(alg=="ab", "ABTesting", ifelse(alg=="thomp","THOMP", ifelse(alg=="thomp_inf", "THOMP_INF", NA) )))
 #data <- read.csv("bias_1_2000.csv")
 
 
@@ -34,22 +35,76 @@ setwd("G:\\My Drive\\Research\\Bandits\\code\\bandits\\analysis\\results")
 
 
 
+df <- group_outcome %>% filter(ite==0) 
+
+
+
+grp = ggplot(df) + geom_point(aes(x=x, y=factor(group)), shape=1, alpha=0.6) +
+  facet_grid(alg ~.) +
+  labs(x='Time Period', y = 'Arm') +
+  theme(axis.text.y = element_text(size = rel(0.7)))  +  theme_bw() + 
+  #ggsave("grp.png", dpi=400, height = 4, width=8, scale = 0.8)
+
+
 df_regret <- regret_mse %>% filter(ite==0)
 
-ggplot(df_regret, aes(x=x, y=regret)) + geom_line(aes(color=alg)) +
-  labs(title = "Regret") + xlim(0,xlimit) +
-  theme_bw() 
+
+reg <- ggplot(df_regret, aes(x=x, y=regret)) + geom_line(aes(color=alg)) +
+  xlim(0,2300) +
+  geom_dl(aes(label=alg), method=list('last.points', cex=0.8)) + theme_minimal() + 
+  theme(legend.position = "none") + 
+  labs(x='Time Period', y = 'Regret') 
+  #ggsave("wise_reg.png", dpi=400, height = 4, width=8, scale = 0.8)
 
 
-df_mse <- regret_mse %>% select(-c(regret, var_mse)) %>% rename(mse = mean_mse) 
+
+
+ggsave("wise_grp_reg.png", arrangeGrob(grp, reg, nrow = 1, ncol=2),
+       dpi=400, height = 4, width=12, scale = 0.9)
+
+df_mse <- regret_mse %>% select(-c(regret, var_mse)) %>% rename(mse = mean_mse) %>% filter(ite==0)
+
+
 
 # %>% 
 #   group_by(x, alg) %>%
 #   summarise(mn_mse = mean(mse), se_mse = sd(mse)/sqrt(n()))
 
 
-ggplot(df_mse %>% filter(ite==0),aes(x=x, y=mse)) +
-  geom_line(aes(color=alg)) + 
-  #geom_ribbon(aes(ymin=mn_mse-(1.96*se_mse), ymax=mn_mse+(1.96*se_mse),group=alg), alpha=0.4,  fill="grey70") +
-  labs(title = "MSE of Mean") +  xlim(0,xlimit) + 
-  theme_bw() 
+ggplot(df_mse, aes(x=x, y=mse)) + geom_line(aes(color=alg)) +
+  xlim(0,2150) + ylim(0,0.6) +
+  geom_dl(aes(label=alg), method=list('last.points', cex=0.8)) + theme_minimal() + 
+  theme(legend.position = "none") + 
+  labs(x='Time Period', y = 'MSE of Mean') + 
+  ggsave("wise_mn.png", dpi=400, height = 5, width=10, scale = 0.8)
+
+
+df_mse <- regret_mse %>% select(-c(regret, var_mse)) %>% rename(mse = mean_mse) %>% filter(ite==0)
+
+# %>% 
+#   group_by(x, alg) %>%
+#   summarise(mn_mse = mean(mse), se_mse = sd(mse)/sqrt(n()))
+
+
+mn <- ggplot(df_mse, aes(x=x, y=mse)) + geom_line(aes(color=alg)) +
+  xlim(0,2500) + ylim(0,1.15) +
+  geom_dl(aes(label=alg), method=list('last.points', cex=0.8)) + theme_minimal() + 
+  theme(legend.position = "none") + 
+  labs(x='Time Period', y = 'MSE of Mean') 
+
+
+df_mse <- regret_mse %>% select(-c(regret, mean_mse)) %>% rename(mse = var_mse) %>% filter(ite==0)
+
+
+
+var <- ggplot(df_mse, aes(x=x, y=mse)) + geom_line(aes(color=alg)) +
+  xlim(0,2500) + ylim(0,1.8) +
+  geom_dl(aes(label=alg), method=list('last.points', cex=0.8)) + theme_minimal() + 
+  theme(legend.position = "none") + 
+  labs(x='Time Period', y = 'MSE of Variance') 
+
+par(mfrow = c(1,2))
+
+ggsave("wise_mn_var.png", arrangeGrob(mn, var, nrow = 1, ncol=2),
+       dpi=400, height = 4, width=10, scale = 0.8)
+
