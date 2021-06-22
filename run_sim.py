03f9ed2eb075bc60acc6a169f31cab9420339a8b
Sandeep Kumar Gangarapu <sandeepgangarapu.iitkgp@gmail.com>
2021-06-22 16:45:39 -0500

    change to xi in thomp_inf

--- run_sim.py
+++ run_sim.py
@@ -7,9 +7,9 @@ import pandas as pd
 def run_sim(file_path, true_means, true_vars=None, dist_type='Normal',
             xi=0.005):
     start_time = time.time()
-    #alg_list=['thomp_bern_batched', 'thomp_inf_bern_batched', 'thomp_bern',
-    #          'thomp_inf_bern', 'ab_bern']
-    alg_list = ['thomp_inf_bern', 'thomp_inf_bern_batched']
+    alg_list=['thomp_bern_batched', 'thomp_inf_bern_batched', 'thomp_bern',
+              'thomp_inf_bern', 'ab_bern']
+    # alg_list = ['thomp_inf_bern', 'thomp_inf_bern_batched']
 
     estimator_list = ['eval_aipw']
 
@@ -57,7 +57,7 @@ if __name__ == '__main__':
     if bernoulli_analysis:
         true_means = np.array([0.37098621, 0.33080171, 0.1699615 , 0.18902466, 0.6743146])
         true_vars = true_means*(1-true_means)
-        run_sim('analysis/output/thomp_inf_bern_less_xi.csv', true_means,
+        run_sim('analysis/output/agg_analysis_batched.csv', true_means,
                 true_vars=true_vars, dist_type='Bernoulli')
     if lsn:
         true_means = [1, 1.1, 1.2]
